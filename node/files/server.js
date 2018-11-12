8cacc18e5520c77626cf6b99fc6bfa0cf9646252
xuyang1638 <2786627598@qq.com>
2018-11-12 14:02:22 +0800

    Create Front Server Program

--- node/files/server.js
+++ node/files/server.js
@@ -1,5 +1,8 @@
-let  fs = require('fs');
-let  join = require('path').join;
+let fs = require('fs');
+let join = require('path').join;
+let http = require('http');
+let url = require('url');
+
 /**
  * 
  * @param startPath  起始目录文件夹路径
@@ -28,12 +31,37 @@ function getFileContent(fname) {
     return content;
 }
 
-function outFiles() {
-    let fileNames = findSync('./file/');
-    for(var i = 0;i<fileNames.length;i++){
-        console.log(getFileContent(fileNames[i]));
-    }
+function getHtmlCode(fname) {
+    // Get Href Code
+    let code = '<a href=' + fname + ' >' + fname + '</a>';
+    return code;
 }
 
+function writeCodeInHtml() {
+    //
+}
+
+function startHttpServer(body) {
+    // Get HTML Page Content
+    let head = fs.readFileSync('./server/front/head.html');
+    let foot = fs.readFileSync('./server/front/foot.html');
+    let html = head + body + foot;
+    // Start HTTP Server by Http Module
+    http.createServer(function (request, response) {
+        // Send Http Header
+        // 200 OK
+        // Content type: text/html
+        response.writeHead(200, {'Content-Type': 'text/html'});
+
+        // Send Response Data
+        response.write(html);
+        // Test Response
+        response.end();
+    }).listen(8888);
+    console.log('Server start at 127.0.0.1:8888');
+}
+
+function main() {}
+
 // Output Test Infomation
-outFiles();
+startHttpServer();
---
+++ node/files/server/front/main.js
@@ -0,0 +1,3 @@
+function getViewCode(fname) {
+    var //
+}
